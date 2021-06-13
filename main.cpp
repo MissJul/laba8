34e017c1ec3a590d136af0c98e3262abfa436604
Yulia <yulianawaseluk69@gmail.com>
2021-06-13 21:30:03 +0300

    1.3.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+void input(int*,int*);
 int main()
 {
-
+    setlocale(LC_ALL, "rus");
+     int num1, num2;
+     input(&num1, &num2);
+     cout << "„исла:"<< num1 << ";" << num2 << end;
     return 0;
 }
-
+ void input(int *a, int *b)
+ {
+     cout << "¬ведите первое число:";
+     cin >> *a;
+     cout << "¬ведите второе число:";
+     cin >> *b;
+ }
